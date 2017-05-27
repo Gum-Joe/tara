1cab646381867d1c1f87a2b01166bf72f8f30487
Gum Joe <kishansambhi@hotmail.co.uk>
2017-05-27 20:49:44 +0100

    :art: Added selection stuff
    :art: :lipstick: Added ability to select a folder or file

--- src/packages/tara-explorer/components/show-dir.jsx
+++ src/packages/tara-explorer/components/show-dir.jsx
@@ -3,6 +3,7 @@
  * @module explorer
  */
 import React, { Component } from "react";
+import camelcase from "camelcase";
 import PropTypes from "prop-types";
 import { join } from "path";
 import { readdir, statSync } from "fs";
@@ -48,22 +49,33 @@ export default class Dir extends Component {
       }
     });
   }
+  /**
+   * Handle the onClick event
+   * @param {String} id ID to look for
+   * @returns {Function} onClick Event handler
+   */
+  handleOnClick(id) {
+    return () => {
+      // Handle
+      document.getElementById(id).className += " file-highlighted";
+    };
+  }
   render() {
     return (
       <Grid className="files">
         <Grid.Row>
           <Grid.Column size={2}>
             {
-              this.state.contents.map(file => statSync(join(this.props.dir, file)).isDirectory() ? (
-                <div className="file-wrapper">
+              this.state.contents.map(file => statSync(join(this.props.dir, file)).isDirectory() ? ( // Check if path is dir
+                <div id={camelcase(file)} role="presentation" className="file-wrapper" onClick={this.handleOnClick(camelcase(file))}>
                   <FontAwesome name="folder" />
                   <p>{getFileName(file)}</p>
                 </div>
               ) : null)
             }
             {
-              this.state.contents.map(file => !statSync(join(this.props.dir, file)).isDirectory() ? (
-                <div className="file-wrapper">
+              this.state.contents.map(file => !statSync(join(this.props.dir, file)).isDirectory() ? ( // Check if path is not dir
+                <div id={camelcase(file)} role="presentation" className="file-wrapper" onClick={this.handleOnClick(camelcase(file))}>
                   <FontAwesome name="file" />
                   <p>{getFileName(file)}</p>
                 </div>
