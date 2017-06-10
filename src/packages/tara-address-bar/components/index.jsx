3fa4e362857b0751a6b4cb01abcc5c413a54f191
Gum Joe <kishansambhi@hotmail.co.uk>
2017-06-10 12:22:45 +0100

    :lipstick: one-fluent-theme changes
    :lipstick: one-fluent-theme changes:
    :lipstick: Added more backgrounds to use
    :lipstick: Added background bluring for acyrlic look
    :lipstick: Changed opacity of explorer & address bar backgrounds
    :art: Added address bar action buttons & styles
    :art: We now have a functioning file explorer for just browsing at the moment.

--- src/packages/tara-address-bar/components/index.jsx
+++ src/packages/tara-address-bar/components/index.jsx
@@ -3,12 +3,27 @@
  * @module address-bar
  */
 import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+import jquery from "jquery";
 import PropTypes from "prop-types";
 
 export default class TaraAddressBar extends Component {
+  componentDidMount() {
+    // Handle action click
+    jquery(".tara-address-bar .fa-arrow-left").click(() => {
+      global.explorerHistory.goBack();
+    });
+    jquery(".tara-address-bar .fa-arrow-right").click(() => {
+      global.explorerHistory.goForward();
+    });
+  }
   render() {
     return (
-      <p>{this.props.dir.dir}</p>
+      <div>
+        <FontAwesome name="arrow-left" />
+        <FontAwesome name="arrow-right" />
+        <p className="current-dir">{this.props.dir.dir}</p>
+      </div>
     );
   }
 }
