5581ba70fcb5c992357dec8e635040e44812bf7c
Kishan <kishansambhi@hotmail.co.uk>
2018-02-14 19:46:11 +0000

    :rocket: Docker :rocket: Added Docker :rocket: Added build script (scripts/bui;d.sh

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+# Tara build script
+# Installs and compiles modules
+# TODO: use Gulp
+
+# Vars & functions
+export NODE_ENV="development"
+
+echo "Building tara..."
+echo "Building root..."
+yarn
+
+echo "Building packages..."
+cd ./src/packages
+echo "Packages:"
+ls
+echo ""
+cd tara-address-bar
+yarn && yarn run build
+cd tara-core
+yarn && yarn run build
+cd tara-explorer
+yarn && yarn run build
+cd tara-file-operations
+yarn && yarn run build
+cd tara-right-click-menu
+yarn && yarn run build
+
+echo "Modules done, bootstrapping..."
+cd ../..
+yarn run bootstrap
